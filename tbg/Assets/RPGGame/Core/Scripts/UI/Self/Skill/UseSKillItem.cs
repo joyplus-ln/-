f3f7e1debfdf83f0df91379dfe44d58a48422af1
lvjibin <lvjibin@fotoable.com>
2018-09-26 12:31:17 +0800

    add

---
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/Skill/UseSKillItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSKillItem : MonoBehaviour
+{
+    private UseSkillManager manager;
+    public void SetData(UseSkillManager manager)
+    {
+        this.manager = manager;
+    }
+    public void PointDown()
+    {
+        Debug.Log("point down");
+        manager.SelectedTransform.SetParent(transform, false);
+    }
+}
---
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/Skill/UseSkillManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSkillManager : MonoBehaviour
+{
+    public List<UseSKillItem> useSKillItems = new List<UseSKillItem>();
+    public Transform SelectedTransform;
+    // Use this for initialization
+    void Start()
+    {
+        SetData(5);
+    }
+
+    public void SetData(int num)
+    {
+        for (int i = 0; i < useSKillItems.Count; i++)
+        {
+            if (i < num)
+            {
+                useSKillItems[i].SetData(this);
+                useSKillItems[i].gameObject.SetActive(true);
+            }
+            else
+            {
+                useSKillItems[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+}
