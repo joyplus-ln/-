03c3929adb4df068c2e76e2c48b2101586177072
lvjibin <lvjibin@fotoable.com>
2018-09-27 22:07:01 +0800

    add

---
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/CharacterListUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterListUI : MonoBehaviour
+{
+
+    public GameObject item;
+    public Transform content;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(CreatItem());
+    }
+
+    IEnumerator CreatItem()
+    {
+        yield return null;
+        GameObject items = null;
+        foreach (var key in PlayerItem.characterDataMap.Keys)
+        {
+            yield return items = Instantiate(item);
+            items.transform.SetParent(content, false);
+        }
+
+    }
+
+}
