a2a0e58b410f6166c6b5ce203ce05abf035f58d3
lvjibin <lvjibin@fotoable.com>
2018-09-28 09:38:38 +0800

    add

--- tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/EquipListItem.cs
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/EquipListItem.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class EquipListItem : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    private EquipListUI listui;
+    private PlayerItem equipItem;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public void Init(EquipListUI listui, PlayerItem equipItem)
+    {
+        this.listui = listui;
+        this.equipItem = equipItem;
+    }
+
+    public void PointDown()
+    {
+        Click();
+    }
+
+    public void Click()
+    {
+        listui.SelectedGameObject.transform.SetParent(transform, false);
+        listui.SelectedGameObject.GetComponent<RectTransform>().anchorMin = Vector2.zero;
+        listui.SelectedGameObject.GetComponent<RectTransform>().anchorMax = Vector2.one;
+        listui.SelectedItem(this, equipItem);
+    }
 }
--- tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/EquipListUI.cs
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/EquipListUI.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EquipListUI : MonoBehaviour {
+public class EquipListUI : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    public GameObject item;
+    public Transform content;
+    public GameObject SelectedGameObject;
+    private EquipListItem currentSelectedItem;
+    public AttributeShow AttributeShowScript;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(CreatItem());
+    }
+
+    IEnumerator CreatItem()
+    {
+        yield return null;
+        GameObject items = null;
+        foreach (var key in PlayerItem.equipDataMap.Keys)
+        {
+            yield return items = Instantiate(item);
+            items.GetComponent<EquipListItem>().Init(this, PlayerItem.equipDataMap[key]);
+            items.transform.SetParent(content, false);
+        }
+
+    }
+
+    public void SelectedItem(EquipListItem currentSelectedItem, PlayerItem selectedItem)
+    {
+        this.currentSelectedItem = currentSelectedItem;
+        AttributeShowScript.SetupInfo(selectedItem.Attributes);
+    }
 }
