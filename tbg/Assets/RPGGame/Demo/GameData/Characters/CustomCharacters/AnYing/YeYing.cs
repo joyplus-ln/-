3089afd1be4b463b165171c524767ae641316205
USERCHI-PESMD71\Administrator <517752548@qq.com>
2018-10-28 18:23:04 +0800

    add

---
+++ tbg/Assets/RPGGame/Demo/GameData/Characters/CustomCharacters/AnYing/YeYing.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YeYing
+{
+
+}
+
+public class DingXiangZhuiJi : CustomSkill
+{
+
+    public DingXiangZhuiJi()
+    {
+        des = "对敌人定向追击，并造成120%伤害，40%几率追击成功，如果追击成功，额外使用一次技能";
+        skillName = "定向追击";
+        spendPower = 5;
+    }
+
+    public override IEnumerator DoSkillLogic()
+    {
+        if (selfOnly.ActionTarget.Hp > 0)
+        {
+            yield return MoveToTarget();
+            AttackTarget(new SkillAttackDamage(0, 1.2f, 0));
+            yield return MoveToSelfPos();
+            if (Random.Range(0, 11) < 5)
+            {
+                yield return DoSkillLogic();
+            }
+        }
+        else
+        {
+            List<BaseCharacterEntity> enemy = GetRandomEnemy(0);
+            if (enemy.Count > 0)
+            {
+                selfOnly.ActionTarget = enemy[0] as CharacterEntity;
+                yield return DoSkillLogic();
+            }
+
+        }
+    }
+}
---
+++ tbg/Assets/RPGGame/Demo/GameData/Characters/CustomCharacters/SenLin/FengZhiYinLv.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FengZhiYinLv
+{
+
+
+}
+
+public class FengZhiYunSha : CustomSkill
+{
+    public FengZhiYunSha()
+    {
+        spendPower = 12;
+        des = "对所有敌人造成90%伤害,如果敌人死亡,追加一次技能";
+        skillName = "风之陨杀";
+    }
+
+    public override IEnumerator DoSkillLogic()
+    {
+        yield return MoveToTarget();
+        List<BaseCharacterEntity> enemys = GetRandomEnemy(99);
+        if (enemys.Count > 0)
+        {
+            bool die = false;
+            for (int i = 0; i < enemys.Count; i++)
+            {
+                AttackInfo info = AttackTarget(new SkillAttackDamage(0, 0.9f, 0), enemys[i] as CharacterEntity);
+                if (info.die) die = true;
+            }
+            if (die)
+                yield return DoSkillLogic();
+        }
+        yield return MoveToSelfPos();
+    }
+}
