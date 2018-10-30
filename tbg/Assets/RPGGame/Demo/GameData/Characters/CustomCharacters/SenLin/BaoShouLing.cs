37df125a6b5595d82e597ab8ddf2d18d971cb9e9
lvjibin <lvjibin@fotoable.com>
2018-10-30 20:40:48 +0800

    add

---
+++ tbg/Assets/RPGGame/Demo/GameData/Characters/CustomCharacters/SenLin/BaoShouLing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaoShouLing
+{
+
+}
+
+public class ShiJianZhuanYi : CustomSkill
+{
+    public ShiJianZhuanYi()
+    {
+        des = "";
+        skillName = "";
+        spendPower = 10;
+        usageScope = SkillUsageScope.Ally;
+    }
+
+    public override IEnumerator DoSkillLogic()
+    {
+        yield return MoveSkillOB(() =>
+        {
+            selfOnly.ActionTarget.currentTimeCount = 99999;
+        });
+    }
+}
---
+++ tbg/Assets/RPGGame/Demo/GameData/Characters/CustomCharacters/YouAn/KuangZhanShi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KuangZhanShi
+{
+
+}
+
+public class ShiXue : CustomSkill
+{
+    public ShiXue()
+    {
+        des = "";
+        skillName = "";
+        skilltype = SkillType.passive;
+        spendPower = 0;
+    }
+
+    public override void BeforeFight()
+    {
+        SelfAttributes._mAtkRate = (1 - (selfOnly.Hp / selfOnly.MaxHp));
+    }
+}
