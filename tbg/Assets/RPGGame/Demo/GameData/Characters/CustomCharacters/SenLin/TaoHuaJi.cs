1eff8ebd7139bd1b0a65cabf387c0e88256ba4a3
lvjibin <lvjibin@fotoable.com>
2018-11-08 09:09:28 +0800

    add

---
+++ tbg/Assets/RPGGame/Demo/GameData/Characters/CustomCharacters/SenLin/TaoHuaJi.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaoHuaJi
+{
+
+
+}
+
+public class TaoHuaZhuoZhuo : CustomSkill
+{
+    public TaoHuaZhuoZhuo()
+    {
+        skillName = "";
+        des = "";
+        spendPower = 16;
+    }
+
+    public override IEnumerator DoSkillLogic()
+    {
+        yield return MoveSkillOB(() =>
+        {
+            selfOnly.ActionTarget.Custombody.AddFlood((int)(selfOnly.MaxHp * 0.2f));
+        });
+    }
+}
