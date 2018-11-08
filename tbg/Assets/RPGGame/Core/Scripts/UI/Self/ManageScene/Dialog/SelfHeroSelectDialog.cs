0465f732aeb894d0960209a9069eb87f372c007d
lvjibin <lvjibin@fotoable.com>
2018-11-08 09:33:50 +0800

    add

---
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/Dialog/SelfHeroSelectDialog.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SelfHeroSelectDialog : Dialog
+{
+
+
+
+    public UIAttributeShow AttributeShow;
+    private SelfHeroSelectData shopItemData;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    public override void Init(DialogData data)
+    {
+        base.Init(data);
+        shopItemData = (SelfHeroSelectData)data.obj;
+        AttributeShow.SetupInfo(PlayerItem.characterDataMap[shopItemData.heroGuid].GetItemAttributes());
+        List<PlayerItem> equipments = PlayerItem.equipDataMap.Values.ToList().FindAll(x => x.EquipItemGuid == shopItemData.heroGuid);
+
+    }
+
+    public void ClickEquip()
+    {
+    }
+
+
+}
+
+public class SelfHeroSelectData
+{
+    public string heroGuid;
+}
