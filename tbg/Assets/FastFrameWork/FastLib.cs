2242436028b27bacb578c065a9cf8f3023d1f18c
lvjibin <lvjibin@fotoable.com>
2018-11-23 18:40:04 +0800

    add new fastLib

---
+++ tbg/Assets/FastFrameWork/FastLib.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CatLib;
+using UnityEngine;
+
+public class FastLib : FastMonoSingleton<FastLib>
+{
+    public override void Init()
+    {
+        base.Init();
+        DontDestroyOnLoad(this);
+        Debug.Log("FastLib-Init");
+    }
+
+    public void Open()
+    {
+        
+    }
+}
---
+++ tbg/Assets/FastFrameWork/TestScript/TestFastLib.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFastLib : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		FastLib.instance.Open();
+    }
+	
+	
+}
