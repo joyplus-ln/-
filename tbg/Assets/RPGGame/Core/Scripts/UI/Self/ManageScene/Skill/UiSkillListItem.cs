36e38128d5d58815e4c798e84735d023c6482f32
USERCHI-PESMD71\Administrator <517752548@qq.com>
2018-11-25 15:35:56 +0800

    add

--- tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/Skill/UiSkillListItem.cs
+++ tbg/Assets/RPGGame/Core/Scripts/UI/Self/ManageScene/Skill/UiSkillListItem.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,16 +11,32 @@ public class UiSkillListItem : MonoBehaviour
     private CustomSkill currentSkill;
 
     public Text skillName;
+
+    public Action<string> clickCallBack;
     // Use this for initialization
     void Start()
     {
 
     }
 
-    public void SetData(CustomSkill currentSkill)
+    public void SetData(CustomSkill currentSkill, Action<string> clickCallBack)
     {
         this.currentSkill = currentSkill;
         skillName.text = currentSkill.skillName;
+        this.clickCallBack = clickCallBack;
     }
 
+    public void Click()
+    {
+        StringBuilder build = new StringBuilder();
+
+        build.AppendLine("技能：" + currentSkill.skillName);
+        build.AppendLine("类型：" + currentSkill.skilltype.ToString());
+        build.AppendLine("能量消耗:" + currentSkill.spendPower.ToString());
+        build.AppendLine("效果:" + currentSkill.des);
+        if (clickCallBack != null)
+        {
+            clickCallBack.Invoke(build.ToString());
+        }
+    }
 }
