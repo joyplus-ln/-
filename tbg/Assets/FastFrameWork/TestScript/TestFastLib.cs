d0a48e572f691dd15c171313cbffe5a01cbbfe2b
lvjibin <lvjibin@fotoable.com>
2018-11-27 15:53:26 +0800

    add

--- tbg/Assets/FastFrameWork/TestScript/TestFastLib.cs
+++ tbg/Assets/FastFrameWork/TestScript/TestFastLib.cs
@@ -8,7 +8,7 @@ public class TestFastLib : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        FastLib.instance.Open();
+        //FastLib.instance.Open();
     }
 
 
---
+++ tbg/Assets/FastFrameWork/Utils/FastContent.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastContent {
+
+    /// <summary>
+    /// bundle的后缀名
+    /// </summary>
+    public const string BundleSuffix = ".unity3d";
+}
